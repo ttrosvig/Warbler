5f0535132d2fcf03fe958cd22ed7f365f30819a2
Tristan <ttrosvig804@gmail.com>
2020-08-19 11:15:28 -0700

    Fixed anchor tag overlap that prevented a like

--- test_user_model.py
+++ test_user_model.py
@@ -1,26 +1,25 @@
 """User model tests."""
 
 # run these tests like:
-#
-#    python -m unittest test_user_model.py
-
-
-import os
+# python -m unittest test_user_model.py
+# import os
 from unittest import TestCase
-
 from models import db, User, Message, Follows
+from secret import PASSWORD
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
 # before we import our app, since that will have already
 # connected to the database
 
-os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
+# os.environ['DATABASE_URL'] = f'postgresql://postgres:{PASSWORD}@localhost/warbler-test'
 
 
 # Now we can import app
 
 from app import app
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:{PASSWORD}@localhost/warbler-test'
 
 # Create our tables (we do this here, so we only create the tables
 # once for all tests --- in each test, we'll delete the data
@@ -56,3 +55,5 @@ class UserModelTestCase(TestCase):
         # User should have no messages & no followers
         self.assertEqual(len(u.messages), 0)
         self.assertEqual(len(u.followers), 0)
+
+    
